c571dd46aae57cdc05fc1af53543b4765b68ddb8
Chris Daly <chrisdaly1988@gmail.com>
2022-11-01 17:59:59 +0000

    fixed map label margin

--- src/params.js
+++ src/params.js
@@ -3,7 +3,7 @@ export const gateway = 20;
 export const season = 13;
 
 export const maps = {
-  S13PAINTEDWORLD: "Painted World",
+  // S13PAINTEDWORLD: "Painted World",
   s13Ferocityv1_2: "Ferocity",
   s13WellspringTemplev1_2: "Wellspring Temple",
   s13EkrezemsMazev1_1: "Ekrezems Maze",
@@ -12,4 +12,7 @@ export const maps = {
   s13_1OrdealGroundv1_07: "Ordeal Ground",
   s13GoldRush: "Gold Rush",
   s13RoyalGardensv1_2: "Royal Gardens",
+  s13NerubianPassage: "Nerubian Passage",
+  s13PaintedWorld: "Painted World",
+  s13Nightopia: "Nightopia",
 };
