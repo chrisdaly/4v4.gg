6ecb68eb75afd28a01d330771cd8c13edc975f39
chrisdaly <chrisdaly1988@gmail.com>
2024-04-21 18:49:59 +0400

    merge

--- src/params.js
+++ src/params.js
@@ -1,20 +1,20 @@
 export const gameMode = 4;
 export const gateway = 20;
-export const season = 17;
+export const season = 18;
 
 export const maps = {
-	s13TwilightRuinsLV: "Twilight Ruins",
-	s13Ferocityv1_2: "Ferocity",
-	s13WellspringTemplev1_2: "Wellspring Temple",
-	s13EkrezemsMazev1_1: "Ekrezems Maze",
-	s13Snowblindv1_2: "Snowblind",
-	s13NorthshireLV: "Northshire",
-	s13_1OrdealGroundv1_07: "Ordeal Ground",
-	s13GoldRush: "Gold Rush",
-	s13RoyalGardensv1_2: "Royal Gardens",
-	s13NerubianPassage: "Nerubian Passage",
-	s13PaintedWorld: "Painted World",
-	s13Nightopia: "Nightopia",
-	s13_1IndigoKeeperv1_1: "Indigo Keeper",
-	s13DeadlockLV: "Deadlock",
+  s13TwilightRuinsLV: "Twilight Ruins",
+  s13Ferocityv1_2: "Ferocity",
+  s13WellspringTemplev1_2: "Wellspring Temple",
+  s13EkrezemsMazev1_1: "Ekrezems Maze",
+  s13Snowblindv1_2: "Snowblind",
+  s13NorthshireLV: "Northshire",
+  s13_1OrdealGroundv1_07: "Ordeal Ground",
+  s13GoldRush: "Gold Rush",
+  s13RoyalGardensv1_2: "Royal Gardens",
+  s13NerubianPassage: "Nerubian Passage",
+  s13PaintedWorld: "Painted World",
+  s13Nightopia: "Nightopia",
+  s13_1IndigoKeeperv1_1: "Indigo Keeper",
+  s13DeadlockLV: "Deadlock",
 };
